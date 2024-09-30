46a4fbb38145f4462dad3f248dba87f86b5400a5
AMateoSernaG <amsernag@udistrital.edu.co>
2024-09-30 07:47:25 -0500

    Tarea Calculadora
    Tarea correspondiente a calculadora con archivo Clase y Main

---
+++ Corte1/main_calc.py
@@ -0,0 +1,11 @@
+import Clase_calc
+
+# Iniciar calculadora
+inicio = Clase_calc.Ini(1)
+inicio.getval()
+
+# Crear objeto de la clase que realiza las operaciones
+calculadora = Clase_calc.Add_Sub()
+
+# Ejecutar la calculadora
+calculadora.ejecutar()
